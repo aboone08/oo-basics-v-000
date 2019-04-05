8c8623a6fc2780bf822f2ec37cdc81d8f86a7529
Andranique Boone <andranique.boone@gmail.com>
2019-04-05 02:19:59 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,9 +2,13 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  
+
   def initialize(brand)
     @brand = brand
+  end
+
+  def cobble
+    puts "shoe has been repaired."
   end
 
 end
